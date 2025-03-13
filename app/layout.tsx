7cbcd0f6f9be1d991902d19db92bc0b3051de08b
Dario Rapisardi <dariorapisardi@users.noreply.github.com>
2025-03-13 10:38:10 -0700

    Add favicon of Sammie cartoon

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata = {
   title: "404 Love Found",
   description: "Find love when you're lost",
+  icons: {
+    icon: "/favicon.ico",
+  },
 }
 
 export default function RootLayout({
